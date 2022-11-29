16b3116239eaaedeac4c962dd3ce7322c0178dc6
Guest1 <guest1@Taehoons-MacBook-Air.local>
2022-11-28 23:01:11 -0500

    Update database schema

--- src/database/schema.sql
+++ src/database/schema.sql
@@ -1,23 +1,23 @@
 CREATE TABLE account (
-	id BIGSERIAL NOT NULL PRIMARY KEY,
-	username VARCHAR(50) NOT NULL UNIQUE,
-	pass VARCHAR(320) NOT NULL,
-	phone_number VARCHAR(20) NOT NULL UNIQUE,
-	preferred_streaming_platform VARCHAR(20) NOT NULL
+	id bigserial NOT NULL PRIMARY KEY,
+	username varchar(25) NOT NULL UNIQUE,
+	pass varchar(320) NOT NULL,
+	phone_number varchar(20) NOT NULL UNIQUE,
+	preferred_streaming_platform varchar(20) NOT NULL
 );
 
 CREATE TABLE refresh_token (
-	id BIGSERIAL NOT NULL PRIMARY KEY,
-	username VARCHAR(50) NOT NULL,
-	token VARCHAR(320) NOT NULL
+	id bigserial NOT NULL PRIMARY KEY,
+	username varchar(25) NOT NULL,
+	token varchar(320) NOT NULL
 );
 
 CREATE TYPE friendship_status AS ENUM ('friends', 'requested')
 
 CREATE TABLE friendship (
-	id BIGSERIAL NOT NULL PRIMARY KEY,
-	account_1 BIGINT REFERENCES account(id),
-	account_2 BIGINT REFERENCES account(id),
-	current_status relationship_status NOT NULL,
-	UNIQUE (account_1, account_2)
+	id bigserial NOT NULL PRIMARY KEY,
+	requester_id bigint REFERENCES account(id),
+	recipient_id bigint REFERENCES account(id),
+	current_status friendship_status NOT NULL,
+	UNIQUE (requester_id, recipient_id)
 );
