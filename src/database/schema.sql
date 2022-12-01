46b9a779eacd3b20f4b54f056bce0189e29ddaf1
Chris Yu <chris.b.yu@gmail.com>
2022-11-30 20:02:47 -0500

    added session and session_user tables to schema

--- src/database/schema.sql
+++ src/database/schema.sql
@@ -10,3 +10,21 @@ CREATE TABLE refresh_token (
 	account_id bigint REFERENCES account(id) PRIMARY KEY,
 	token varchar(320) NOT NULL
 );
+
+CREATE TYPE platform_type AS ENUM ('Apple Music', 'Spotify');
+
+CREATE TABLE musix_session (
+  id varchar(6) PRIMARY KEY,
+  name varchar(25) NOT NULL,
+  host_id bigint REFERENCES account(id),
+  date varchar(10) NOT NULL,
+  platform platform_type NOT NULL,
+  track_ids text[],
+  completed boolean NOT NULL
+);
+
+CREATE TABLE musix_session_user (
+  account_id bigint REFERENCES account(id),
+  musix_session_id varchar(6) REFERENCES musix_session(id),
+  PRIMARY KEY(account_id, musix_session_id)
+);
