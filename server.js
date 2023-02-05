b92b0b6d3c82c910cff5e02aeb857701f93fd540
Katy Penquite <cpenquit@katymac.local>
2023-02-05 00:21:27 -0500

    move secrets to AWS secret manager

--- server.js
+++ server.js
@@ -1,21 +1,47 @@
 // http://localhost:3000
 const fs = require('fs')
-const crypto = require('crypto')
+const awsSecretsManager = require('@aws-sdk/client-secrets-manager')
+
+const getSecrets = async () => {
+	// If you need more information about configurations or implementing the sample code, visit the AWS docs:
+	// https://docs.aws.amazon.com/sdk-for-javascript/v3/developer-guide/getting-started.html
+
+	const secret_name = "Auxio_server_secrets";
+
+	const client = new awsSecretsManager.SecretsManagerClient({
+		region: "us-east-2",
+	});
+
+	let response;
+
+	try {
+		response = await client.send(
+			new awsSecretsManager.GetSecretValueCommand({
+				SecretId: secret_name,
+				VersionStage: "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified
+			})
+		);
+	} catch (error) {
+		// For a list of exceptions thrown, see
+		// https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+		throw error;
+	}
+
+	const secrets = JSON.parse(response.SecretString);
+	const envContent = "\nACCESS_TOKEN_SECRET=" + secrets.access_token_secret
+		+ "\nREFRESH_TOKEN_SECRET=" + secrets.refresh_token_secret
+		+ "\nCODE_TOKEN_SECRET=" + secrets.code_token_secret 
+		+ "\nPASSWORD_TOKEN_SECRET=" + secrets.password_token_secret
+	fs.appendFileSync('.env', envContent)
+}
+
 if (!fs.existsSync('./.env')) {
-	const accessTokenSecret = crypto.randomBytes(64).toString('hex')
-	const refreshTokenSecret = crypto.randomBytes(64).toString('hex')
-	const codeTokenSecret = crypto.randomBytes(64).toString('hex')
-	const passwordTokenSecret = crypto.randomBytes(64).toString('hex')
 	const twilioAccountSid = "ACd8f103d7ea7aaa411ac76e7929c4836c";
 	const twilioAuthToken = "f29bf59961befe551857738091b4d7fe";
-	const envContent = "ACCESS_TOKEN_SECRET=" + accessTokenSecret
-		+ "\nREFRESH_TOKEN_SECRET=" + refreshTokenSecret
-		+ "\nCODE_TOKEN_SECRET=" + codeTokenSecret 
-		+ "\nPASSWORD_TOKEN_SECRET=" + passwordTokenSecret
-		+ "\nTWILIO_ACCOUNT_SID=" + twilioAccountSid
+	const envContent = "TWILIO_ACCOUNT_SID=" + twilioAccountSid
 		+ "\nTWILIO_AUTH_TOKEN=" + twilioAuthToken
-	console.log(envContent)
 	fs.writeFileSync('.env', envContent)
+	getSecrets()
 }
 
 require('dotenv').config()
@@ -46,7 +72,6 @@ const upload = multer({storage: multer.diskStorage({
         cb(null, file.originalname);
     }
 })});
-
 
 /*
  * Test creating a new postgres database and connecting to it.
